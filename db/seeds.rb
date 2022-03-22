adbb3c7dda0f8c26bd95b03848723f62f9d94def
rquinterosv <rafael.quinterosv@gmail.com>
2022-03-21 22:49:29 -0300

    se agrego faker y 50 ejemplos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do
+    Sale.create(cafe: Faker::Beer.brand, amount: rand(1000..10000), date:
+    Faker::Date.between(from: '2014-09-23', to: '2014-09-25'))
+    end
